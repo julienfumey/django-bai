90cd667adecd62e0f0128cb62271f3a14b6dfe25
Julien FUMEY <julien.fumey@gmail.com>
2025-09-05 17:39:20 +0200

    add comment form

--- ideas/views.py
+++ ideas/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, FormMixin
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
 from . import models
+from . import forms
 
 
 # Create your views here.
@@ -26,9 +27,27 @@ def idea_downvote(request, pk):
     return JsonResponse({"downvote": idea.downvotes})
 
 
-class IdeaDetailView(DetailView):
-    model = 'ideas.Idea'
+class IdeaDetailView(FormMixin, DetailView):
+    model = models.Idea
     template_name = 'ideas/idea_detail.html'
+    form_class = forms.CommentForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if "form" not in context:
+            context["form"] = self.get_form()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()  # récupérer l'article
+        form = self.get_form()
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.idea = self.object  # lier au modèle principal
+            comment.save()
+            return redirect('idea_detail', pk=self.object.pk)
+        else:
+            return self.form_invalid(form)
 
 
 class IdeaCreateView(CreateView):
