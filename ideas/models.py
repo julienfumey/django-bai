d254097013d706f48597e72c20592113f7b53d0e
Julien FUMEY <julien.fumey@gmail.com>
2025-09-05 17:41:43 +0200

    black

--- ideas/models.py
+++ ideas/models.py
@@ -9,14 +9,31 @@ STATUSES = [
 
 
 class Idea(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(
+        max_length=200,
+    )
     description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    upvotes = models.PositiveIntegerField(default=0)
-    downvotes = models.PositiveIntegerField(default=0)
-    answer = models.TextField(blank=True, null=True)
-    status = models.IntegerField(max_length=20, choices=STATUSES, default=0)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+    upvotes = models.PositiveIntegerField(
+        default=0,
+    )
+    downvotes = models.PositiveIntegerField(
+        default=0,
+    )
+    answer = models.TextField(
+        blank=True,
+        null=True,
+    )
+    status = models.IntegerField(
+        max_length=20,
+        choices=STATUSES,
+        default=0,
+    )
 
     def __str__(self):
         return self.title
@@ -31,12 +48,24 @@ class Idea(models.Model):
 
 
 class Comment(models.Model):
-    idea = models.ForeignKey(Idea, related_name='comments', on_delete=models.CASCADE)
+    idea = models.ForeignKey(
+        Idea,
+        related_name='comments',
+        on_delete=models.CASCADE,
+    )
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    upvotes = models.PositiveIntegerField(default=0)
-    downvotes = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+    upvotes = models.PositiveIntegerField(
+        default=0,
+    )
+    downvotes = models.PositiveIntegerField(
+        default=0,
+    )
 
     def __str__(self):
         return f'Comment on {self.idea.title} by {self.created_at}'
