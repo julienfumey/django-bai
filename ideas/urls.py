09ce1e22e25c25993fc3f432633a2b554aac99e0
Julien FUMEY <julien.fumey@gmail.com>
2025-09-23 20:40:40 +0200

    add modal for report of comments

--- ideas/urls.py
+++ ideas/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('idea/<pk>/view/', IdeaDetailView.as_view(), name='idea_detail'),
     path('idea/<pk>/up', idea_upvote, name="idea_upvote"),
     path('idea/<pk>/down', idea_downvote, name="idea_downvote"),
+    path('comment/<pk>/up', comment_upvote, name="comment_upvote"),
+    path('comment/<pk>/down', comment_downvote, name="comment_downvote"),
     path('idea/add/', IdeaCreateView.as_view(), name='idea_create'),
 ]
--- ideas/views.py
+++ ideas/views.py
@@ -52,30 +52,59 @@ def idea_downvote(request, pk):
     return JsonResponse({"downvote": idea.downvotes})
 
 
+def comment_upvote(request, pk):
+    comment = models.Comment.objects.get(pk=pk)
+    comment.upvote()
+    return JsonResponse({"upvote": comment.upvotes})
+
+
+def comment_downvote(request, pk):
+    comment = models.Comment.objects.get(pk=pk)
+    comment.downvote()
+    return JsonResponse({"downvote": comment.downvotes})
+
+
 class IdeaDetailView(DetailView):
     model = models.Idea
     template_name = 'ideas/idea_detail.html'
     context_object_name = "idea"
-    form_class = forms.CommentForm
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["form"] = self.form_class
+        context["comment_form"] = forms.CommentForm()
+        context["report_form"] = forms.ReportForm()
         return context
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()  # récupérer l'article
 
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            comment = models.Comment(idea=self.object, content=form.cleaned_data['content'])
+        if "comment_submit" in request.POST:
+            comment_form = forms.CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = models.Comment(idea=self.object, content=comment_form.cleaned_data['content'])
 
-            if 'answer_to_id' in form.cleaned_data:
-                answer_to = models.Comment.objects.get(pk=form.cleaned_data['answer_to_id'])
+                if 'answer_to_id' in request.POST:
+                    answer_to = models.Comment.objects.get(pk=comment_form.cleaned_data['answer_to_id'])
+                    comment.answer_to = answer_to
 
-                comment.answer_to = answer_to
-            comment.save()
-            return redirect('idea_detail', pk=self.object.pk)
+                comment.save()
+                return redirect('idea_detail', pk=self.object.pk)
+
+        if "report_submit" in request.POST:
+            report_form = forms.ReportForm(request.POST)
+            if report_form.is_valid():
+                reason = report_form.cleaned_data['reason']
+                commentaire = report_form.cleaned_data['comment']
+                type_ = report_form.cleaned_data['type_']
+                id_ = report_form.cleaned_data['id_']
+                if type_ == 'idea':
+                    print("toto")
+                    idea = models.Idea.objects.get(pk=id_)
+                    idea.signaler(reason, commentaire)
+                elif type_ == 'comment':
+                    comment = models.Comment.objects.get(pk=id_)
+                    comment.signaler(reason, commentaire)
+                return redirect(self.request.path)
 
         return self.get(self, request, *args, **kwargs)
         # answer_form = self.get_form()
