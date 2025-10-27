e9b99ec0294f9fe3120361b13e87e2e4bdbe70db
Julien FUMEY <julien.fumey@gmail.com>
2025-10-27 20:25:50 +0100

    add sorting filter

--- ideas/filters.py
+++ ideas/filters.py
@@ -4,10 +4,24 @@ from .models import Idea, STATUSES
 
 class IdeaFilter(django_filters.FilterSet):
     status = django_filters.ChoiceFilter(label='Status')
+    order = django_filters.OrderingFilter(
+        fields=(
+            ('status', 'status'),
+            ('created_at', 'date_creation'),
+            ('upvotes', 'upvotes'),
+            ('downvotes', 'downvotes'),
+        ),
+        field_labels={
+            'status': 'Status',
+            'created_at': 'Date de création',
+            'upvotes': 'Votes positifs',
+            'downvotes': 'Votes négatifs',
+        },
+    )
 
     class Meta:
         model = Idea
-        fields = ['status']
+        fields = ['status', 'order']
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('request').user
